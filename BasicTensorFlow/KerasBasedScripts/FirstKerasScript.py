1cdcc0b5dd0d1f0a970e1ea239bcd6cbc86ececc
Zhu <mozhu@ebay.com>
2017-03-27 23:17:49 -0700

    First Keras scripts

---
+++ BasicTensorFlow/KerasBasedScripts/FirstKerasScript.py
@@ -0,0 +1,27 @@
+'''
+Created on Mar 27, 2017
+
+@author: mozhu
+'''
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+
+
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+data = np.random.random((1000, 100))
+labels = np.random.randint(2, size=(1000, 1))
+
+# Train the model, iterating on the data in batches of 32 samples
+model.fit(data, labels, epochs=10, batch_size=32)
+
+
+
